9be41bdd310c2208502f2c6858e5dfbdc466adae
Giovanni Bonanno <bonannogiovanni@hotmail.com>
2021-03-30 22:33:37 +0200

    edited proxy and server based url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://calm-crag-28547.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
